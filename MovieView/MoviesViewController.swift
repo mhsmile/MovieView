562f8056af72246b6256d271aef0604d6ec7299b
mhsmile <mihwa@macintosh-8.lan>
2017-02-05 17:41:33 -0800

    First commit

--- MovieView/MoviesViewController.swift
+++ MovieView/MoviesViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  MoviesViewController.swift
 //  MovieView
@@ -8,27 +9,47 @@
 
 import UIKit
 import AFNetworking
-
+import MBProgressHUD
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+   
     
     var movies: [NSDictionary]?
     
-    override func viewDidLoad() {
+
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(refreshControl:)), for: UIControlEvents.valueChanged)
+        
+        tableView.insertSubview(refreshControl, at: 0)
+    
         tableView.dataSource = self
         tableView.delegate = self
+   
+
         
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        
         let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
-            if let data = data {
-                if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+            
+            MBProgressHUD.hide(for: self.view, animated: true)
+           
+            
+            if let data = data
+            {
+                if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary
+                {
                     print(dataDictionary)
                     
                     self.movies = dataDictionary["results"] as? [NSDictionary]
@@ -36,23 +57,28 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                     
                 }
             }
+            
+          
         }
+        
         task.resume()
-        
-        
-        
-        
-        
-        
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+    
+    func refreshControlAction(refreshControl : UIRefreshControl)
+    {
+        tableView.reloadData()
+        refreshControl.endRefreshing()
+        
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         
         if let movies = movies {
--- Podfile
+++ Podfile
@@ -1,9 +1,12 @@
 # Uncomment the next line to define a global platform for your project
 platform :ios, '9.0'
 use_frameworks!
+inhibit_all_warnings!
 
 target 'MovieView' do
 pod 'AFNetworking', '~> 3.0'
+pod 'MBProgressHUD'
+
 # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
 
 
